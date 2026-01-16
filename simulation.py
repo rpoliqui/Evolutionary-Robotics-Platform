46cb2878cda94ab1732742432307ce5b4e69e126
rpoli <rpoliquin01@gmail.com>
2026-01-15 22:16:03 -0500

    Tested camera controls

--- simulation.py
+++ simulation.py
@@ -5,7 +5,7 @@ import time
 #__________Simulation Setup__________
 print("====================Starting Simulation====================")
 physicsClient = p.connect(p.GUI)
-p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+#p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 print("===========================================================")
 
 #__________Simulation Loop__________
