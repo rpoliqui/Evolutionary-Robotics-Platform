1cdec3f9142c312c55348bb207c588e6bc39289d
rpoli <rpoliquin01@gmail.com>
2026-01-15 22:13:17 -0500

    Added time control of stepping

--- simulation.py
+++ simulation.py
@@ -1,5 +1,20 @@
+#__________Import Statements__________
 import pybullet as p
+import time
+
+#__________Simulation Setup__________
+print("====================Starting Simulation====================")
 physicsClient = p.connect(p.GUI)
+p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
+print("===========================================================")
+
+#__________Simulation Loop__________
 for step in range(1000):
     p.stepSimulation()
+    print(step)
+    time.sleep(1./240.)
+
+#_________Simulation End__________
+print("=====================Ending Simulation=====================")
 p.disconnect()
+print("===========================================================")
