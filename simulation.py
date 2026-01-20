61d0e2596566cfeab451c6ded61521b88fce1b1b
rpoli <rpoliquin01@gmail.com>
2026-01-20 17:01:31 -0500

    Added Floor Plane to simulation

--- simulation.py
+++ simulation.py
@@ -1,15 +1,24 @@
 #__________Import Statements__________
 import pybullet as p
+import pybullet_data
 import time
 
 #__________Simulation Setup__________
 print("====================Starting Simulation====================")
+# Connect to GUI
 physicsClient = p.connect(p.GUI)
+
+# Define data path for additional objects (Floor Plane)
+p.setAdditionalSearchPath(pybullet_data.getDataPath())
+
+# Uncomment line below to enable debug graphics
 #p.configureDebugVisualizer(p.COV_ENABLE_GUI,0)
 print("===========================================================")
 
 # Define gravity force
 p.setGravity(0,0,-9.8)
+# Define floor normal force
+planeId = p.loadURDF("plane.urdf")
 
 # Load the world file
 p.loadSDF("box.sdf")
