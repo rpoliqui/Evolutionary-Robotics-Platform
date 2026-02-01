544b34de4f05ebe30b3740083a51f9c2714370e6
rpoliqui <rpoliqui@uvm.edu>
2026-02-01 13:33:11 -0500

    Handle None case for contact points
    Add check for None before processing contact points

--- pyrosim/pyrosim.py
+++ pyrosim/pyrosim.py
@@ -48,9 +48,13 @@ def Get_Touch_Sensor_Value_For_Link(linkName):
 
     desiredLinkIndex = linkNamesToIndices[linkName]
 
-    pts = p.getContactPoints()
+    pts = p.getContactPoints() 
 
-    for pt in pts:
+    if pts==None: 
+    
+        return -1 
+    
+    for pt in pts: 
 
         linkIndex = pt[4]
 
