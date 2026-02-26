f80041aa2036c250125155cc3982ffd7c84f6e81
rpoli <rpoliquin01@gmail.com>
2026-02-26 00:01:20 -0500

    Finished Step 54

--- pyrosim/neuralNetwork.py
+++ pyrosim/neuralNetwork.py
@@ -32,6 +32,8 @@ class NEURAL_NETWORK:
         for neuron in self.neurons:
             if self.neurons[neuron].Is_Sensor_Neuron():
                 self.neurons[neuron].Update_Sensor_Neuron()
+            else:
+                self.neurons[neuron].Update_Hidden_Or_Motor_Neuron()
 
 # ---------------- Private methods --------------------------------------
 
--- pyrosim/neuron.py
+++ pyrosim/neuron.py
@@ -23,6 +23,9 @@ class NEURON:
     def Update_Sensor_Neuron(self):
         self.Set_Value(pyrosim.Get_Touch_Sensor_Value_For_Link(self.Get_Link_Name()))
 
+    def Update_Hidden_Or_Motor_Neuron(self):
+        self.Set_Value(0)
+
     def Add_To_Value( self, value ):
 
         self.Set_Value( self.Get_Value() + value )
