115a61c785abfc6448b7f511ee642242f634c2cf
rpoli <rpoliquin01@gmail.com>
2026-03-04 08:22:01 -0500

    Finished recording synapse Demo

--- generate.py
+++ generate.py
@@ -51,10 +51,10 @@ def Generate_Brain():
     pyrosim.Send_Motor_Neuron(name=4, jointName="Torso_FrontLeg")
 
     # __________Create Synapses__________
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=1)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=1)
-    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=1)
-    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=1)
+    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=3, weight=0.5)
+    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=3, weight=-1)
+    pyrosim.Send_Synapse(sourceNeuronName=1, targetNeuronName=4, weight=-1)
+    pyrosim.Send_Synapse(sourceNeuronName=2, targetNeuronName=4, weight=0.5)
 
     # __________Close the Robot File__________
     pyrosim.End()
